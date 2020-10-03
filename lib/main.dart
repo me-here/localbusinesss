63df8358a72348f5edcb4928485c88c35fb3ced8
Mihir Thanekar <mihirthanekar@gmail.com>
2020-10-03 09:32:39 -0700

    Added routing to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:localbusiness/screens/login_screen/login_screen.dart';
+import 'package:localbusiness/screens/map_screen/map_screen.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Local Business Supporter',
-    home: Scaffold(
-      body: Container(
-        child: Text('hello world'),
-      ),
-    ),
+    routes: {
+      '/login': (_) => LoginScreen(),
+      '/map': (_) => MapScreen(),
+    },
+    initialRoute: '/login',
   ));
 }
--- lib/screens/map_screen/map_screen.dart
+++ lib/screens/map_screen/map_screen.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          child: Text('Map Screen'),
+        ),
+      ),
+    );
   }
 }
