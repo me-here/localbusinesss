1f5e7a17a5fa57313f721a2c1cb7cc5c3ffbc959
Mihir Thanekar <mihirthanekar@gmail.com>
2020-10-03 10:49:39 -0700

    Display random pins from set of markers

--- lib/screens/map_screen/views/map_view.dart
+++ lib/screens/map_screen/views/map_view.dart
@@ -4,7 +4,29 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatelessWidget {
-  Completer<GoogleMapController> _controller = Completer();
+  final Completer<GoogleMapController> _controller = Completer();
+  final Set<Marker> _markers = {
+    Marker(
+      markerId: MarkerId("Joe's coffee"),
+      position: LatLng(37.4, -122.08832357078792),
+    ),
+    Marker(
+      markerId: MarkerId("Beth's Hair Salon"),
+      position: LatLng(37.4, -122.0889),
+    ),
+    Marker(
+      markerId: MarkerId("Joe's coffee"),
+      position: LatLng(37.3, -122.08832357078792),
+    ),
+    Marker(
+      markerId: MarkerId("Joe's coffee"),
+      position: LatLng(37.1, -122.08832357078792),
+    ),
+    Marker(
+      markerId: MarkerId("Joe's coffee"),
+      position: LatLng(37.3, -122.09),
+    ),
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +41,7 @@ class MapView extends StatelessWidget {
         onMapCreated: (controller) {
           _controller.complete(controller);
         },
+        markers: _markers,
       ),
     );
   }
