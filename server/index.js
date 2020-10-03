7d5538479f4179a24c1e0536416963f5dc049408
Mihir Thanekar <mihirthanekar@gmail.com>
2020-10-03 15:14:44 -0700

    Refactor server code

--- server/index.js
+++ server/index.js
@@ -1,31 +1,29 @@
-const mongoose = require('mongoose')
-const express = require('express')
-const businessRoute = require('./routes/businessRoute');
-const mapRoute = require('./routes/mapRoute');
-require('dotenv').config({path: '.env'})
+const mongoose = require("mongoose");
+const express = require("express");
+const businessRoute = require("./routes/businessRoute");
+const mapRoute = require("./routes/mapRoute");
+require("dotenv").config({ path: ".env" });
 
 app = express();
-
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
-
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-})
+  console.log(`Listening on port ${port}`);
+});
 
 const uri = process.env.ATLAS_URI;
+console.log("MongoDB Atlas URI: " + uri);
 
-mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
-                            .catch(err => console.log(err));
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .catch((err) => console.log(err));
 
 const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connection to MongoDB successful");
+});
 
-connection.once('open', () => {
-    console.log('Connection to MongoDB successful');
-})
-
-app.use('/business', businessRoute);
-app.use('/map', mapRoute);
-
-// AIzaSyBk4_I6kJt42OHcxf-uyDlx7LUtQSykvWw
+// First level endpoints of server
+app.use("/business", businessRoute);
+app.use("/map", mapRoute);
