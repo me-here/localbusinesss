663b73f3428754e26f61761488517c6ed95b3930
Mihir Thanekar <mihirthanekar@gmail.com>
2020-10-03 10:15:29 -0700

    Got map on screen

--- lib/screens/map_screen/map_screen.dart
+++ lib/screens/map_screen/map_screen.dart
@@ -1,12 +1,23 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatelessWidget {
+  Completer<GoogleMapController> _controller = Completer();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: Text('Map Screen'),
+        child: GoogleMap(
+          initialCameraPosition: CameraPosition(
+            target: LatLng(37.43296265331129, -122.08832357078792),
+            zoom: 14,
+          ),
+          onMapCreated: (controller) {
+            _controller.complete(controller);
+          },
         ),
       ),
     );
