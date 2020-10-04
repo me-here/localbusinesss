227a9b8e7ffd955cc4614ae5a38a52a0ea7d3313
Mihir Thanekar <mihirthanekar@gmail.com>
2020-10-03 19:15:56 -0700

    Scourged the face of the earth of bugs.

---
+++ lib/screens/home/home.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../login_screen/login_screen.dart';
+import '../login_screen/register.dart';
+
+class Authenticate extends StatefulWidget {
+  @override
+  _AuthenticateState createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  bool showSignIn = true;
+  void toggleView() {
+    setState(() => showSignIn = !showSignIn);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showSignIn) {
+      return LoginScreen(toggleView: toggleView);
+    } else {
+      return Register(toggleView: toggleView);
+    }
+  }
+}
