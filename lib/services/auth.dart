80998ed1cf84a978cd904107693404c2edf9be64
RandyHuynh5815 <55762196+RandyHuynh5815@users.noreply.github.com>
2020-10-03 17:37:10 -0700

    Add files via upload

---
+++ lib/services/auth.dart
@@ -0,0 +1,59 @@
+import 'package:Auth/models/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import './database.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // create user obj based on firebase user
+  TheUser _userFromFirebaseUser(User user) {
+    return user != null ? TheUser(uid: user.uid) : null;
+  }
+
+  // auth change user stream
+  Stream<TheUser> get user {
+    return _auth
+        .authStateChanges()
+        //.map((FirebaseUser user) => _userFromFirebaseUser(user));
+        .map(_userFromFirebaseUser);
+  }
+
+  // sign in with email and password
+  Future login(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // register with email and password
+  Future createAccount(
+      String name, String email, String password, bool business) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      User user = result.user;
+
+      await DatabaseService(uid: user.uid).updateUserData(name, business);
+      return _userFromFirebaseUser(user);
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  // sign out
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
