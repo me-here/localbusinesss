09554d39cf38231bfa622b641dc3bba9bfe2702d
Mihir Thanekar <mihirthanekar@gmail.com>
2020-10-03 19:58:47 -0700

    change url

--- lib/repositories/MongoClient.dart
+++ lib/repositories/MongoClient.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:localbusiness/models/Business.dart';
 
 class MongoClient {
-  static const BASE_URL = 'https://make-uc-server.herokuapp.com/';
+  static const BASE_URL = 'https://make-uc-server.herokuapp.com';
   static Future<List<Business>> getNearbyBusinesses() async {
     final res = await http.get('$BASE_URL/business/');
     List<dynamic> businesses = jsonDecode(res.body);
