5357e71665a7bcc363b821f0309e0e1c98013418
parsakmt <38390447+parsakmt@users.noreply.github.com>
2020-10-03 20:19:55 -0700

    Business Sheet to Sliding Up Panel v2

--- lib/views/business_sheet/widgets/header.dart
+++ lib/views/business_sheet/widgets/header.dart
@@ -7,25 +7,12 @@ class Header extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverAppBar(
-      forceElevated: true,
-      backgroundColor: Colors.white,
-      floating: true,
-      snap: true,
-      pinned: true,
-      title: Container(
-        child: Divider(
-          color: Colors.black,
-          thickness: 5,
-          endIndent: MediaQuery
-              .of(context)
-              .size
-              .width * 0.25,
-          indent: MediaQuery
-              .of(context)
-              .size
-              .width * 0.25,
-        ),
+    return Container(
+      child: Divider(
+        color: Colors.black,
+        thickness: 5,
+        endIndent: MediaQuery.of(context).size.width * 0.25,
+        indent: MediaQuery.of(context).size.width * 0.25,
       ),
     );
   }
