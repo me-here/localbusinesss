f78e0edcffcb1fadb0d1fd7829ff78d10ca467d6
parsakmt <38390447+parsakmt@users.noreply.github.com>
2020-11-23 19:40:45 -0800

    MultiProvider to clean up ChangeNotifier Providers in map_screenfinal.dart

--- lib/screens/map_screen/map_screenfinal.dart
+++ lib/screens/map_screen/map_screenfinal.dart
@@ -13,49 +13,54 @@ import '../../services/authfinal.dart';
 
 class MapScreen extends StatelessWidget {
   final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: ChangeNotifierProvider<SheetProvider>(
-          create: (context) => SheetProvider(),
-          builder: (context, child) => ChangeNotifierProvider<MapProvider>(
-            create: (context) =>
-                MapProvider(sheetProvider: context.read<SheetProvider>()),
-            builder: (context, child) => Stack(
-              children: [
-                MapView(),
-                Positioned(
-                  top: 0,
-                  left: 0,
-                  right: 0,
-                  child: SearchBar(),
-                ),
-                Positioned(
-                  top: MediaQuery.of(context).size.height * 0.1,
-                  right: 0,
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.7,
-                    child: RotatedBox(
-                      quarterTurns: -1,
-                      child: Row(
-                        children: [
-                          Expanded(child: LocationSlider()),
-                          IconButton(
-                            icon: Icon(Icons.arrow_circle_up),
-                            iconSize: 30.0,
-                            onPressed: () async {
-                              await _auth.signOut();
-                            },
-                          )
-                        ],
-                      ),
+        child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider<SheetProvider>(
+              create: (context) => SheetProvider(),
+            ),
+            ChangeNotifierProvider<MapProvider>(
+              create: (context) =>
+                  MapProvider(sheetProvider: context.read<SheetProvider>()),
+            ),
+          ],
+          child: Stack(
+            children: [
+              MapView(),
+              Positioned(
+                top: 0,
+                left: 0,
+                right: 0,
+                child: SearchBar(),
+              ),
+              Positioned(
+                top: MediaQuery.of(context).size.height * 0.1,
+                right: 0,
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.7,
+                  child: RotatedBox(
+                    quarterTurns: -1,
+                    child: Row(
+                      children: [
+                        Expanded(child: LocationSlider()),
+                        IconButton(
+                          icon: Icon(Icons.arrow_circle_up),
+                          iconSize: 30.0,
+                          onPressed: () async {
+                            await _auth.signOut();
+                          },
+                        )
+                      ],
                     ),
                   ),
                 ),
-                DraggableBusinessSheet(),
-              ],
-            ),
+              ),
+              DraggableBusinessSheet(),
+            ],
           ),
         ),
       ),
